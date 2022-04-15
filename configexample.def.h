ea43a705da2392e2dc60a7d1cfbfebf435788d0f
Janey <1737941060@qq.com>
2022-04-15 15:46:06 +0800

    merge all and first config

--- configexample.def.h
+++ configexample.def.h
@@ -7,7 +7,7 @@ static const unsigned int gappih    = 7;       /* horiz inner gap between window
 static const unsigned int gappiv    = 7;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 7;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 7;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Symbols Nerd Font:size=14" };
@@ -45,27 +45,14 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
-#include "vanitygaps.c"
-
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "舘",      tile },    /* first entry is default */
 	{ "",      NULL },    /* no layout function means floating behavior */
 	{ "",      monocle },
-	{ "[@]",      spiral },
-	{ "[\\]",     dwindle },
-	{ "H[]",      deck },
-	{ "TTT",      bstack },
-	{ "===",      bstackhoriz },
-	{ "HHH",      grid },
-	{ "###",      nrowgrid },
-	{ "---",      horizgrid },
-	{ ":::",      gaplessgrid },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
-	{ NULL,       NULL },
 };
+
+/* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
@@ -85,9 +72,6 @@ static const char *lightup[]  = { "xbacklight", "-inc","5" ,NULL};
 static const char *volumdown[]  = { "amixer", "-qM","set", "Master","5%-", "umute",NULL };
 static const char *volumup[]  = { "amixer", "-qM", "set", "Master", "5%+", "umute" ,NULL};
 static const char *mutetoggle[]  = { "amixer","-qM", "set", "Master", "toggle" ,NULL};
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
- 
 
 #define XF86XK_AudioLowerVolume 0x1008FF11   /* Volume control down        */
 #define XF86XK_AudioMute        0x1008FF12   /* Mute sound from the system */
@@ -103,8 +87,6 @@ static Key keys[] = {
 	{ 0,						XF86XK_AudioRaiseVolume,          spawn,		   {.v = volumup}},
 	{ 0,						XF86XK_AudioLowerVolume,      spawn,		   {.v = volumdown}},
 	{ 0,						XF86XK_AudioMute,      spawn,		   {.v = mutetoggle}},
-
-	{ Mod1Mask,                       XK_g,  togglescratch,  {.v = scratchpadcmd } },
 	{ Mod1Mask,                    	XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -177,5 +159,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
 
