b859a974dbb26bf06565b627e4bb029b6f9c8bc1
Janey <1737941060@qq.com>
2022-10-30 16:02:26 +0800

    my build dwm

--- config.def.h
+++ config.def.h
@@ -35,7 +35,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", ""};
+static const char *tags[] = { "","","", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -86,7 +86,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_lavender, "-sf", col_gray1, NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_lavender, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lightdown[]  = { "xbacklight", "-dec","5",NULL };
 static const char *lightup[]  = { "xbacklight", "-inc","5" ,NULL};
